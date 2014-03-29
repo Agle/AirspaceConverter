6821430fc811aba9d39112e56cf887c7c94b5d8b
Dikenson <gurt5000@rambler.ru>
2014-03-29 19:49:36 +0400

    task1 inform

--- ConverterGui/Gui/airspace_converter_interface.cpp
+++ ConverterGui/Gui/airspace_converter_interface.cpp
@@ -11,22 +11,22 @@ AirspaceConverterInterface::AirspaceConverterInterface(QWidget *parent) : QWidge
 
 void AirspaceConverterInterface::createGui(){
    // top_lt = new QGrLayout;
-    text_lt = new QTextEdit;
+    text_lt = new QTextEdit;//Создание TextEdit на форме
     leftWgt = new filechooser;
-    spisok_btn = new QComboBox();
-    ok_btn = new QPushButton(QObject::tr("Ok"));
-    main_lt->addWidget(leftWgt);
-    spisok_btn->addItem("dwg");
-    spisok_btn->addItem("dfx");
-    spisok_btn->addItem("txt");
-    main_lt->addWidget(spisok_btn);
+    spisok_btn = new QComboBox();//Создание выпадающего списка на форме
+    ok_btn = new QPushButton(QObject::tr("Ok"));//создание клавиши Ок на форме
+    main_lt->addWidget(leftWgt);//Добавление виджета из filechooser
+    spisok_btn->addItem("dwg");//заполнение выпадающего списка
+    spisok_btn->addItem("dfx");//заполнение выпадающего списка
+    spisok_btn->addItem("txt");//заполнение выпадающего списка
+    main_lt->addWidget(spisok_btn);//расстановка на форме
     main_lt->addWidget(ok_btn);
     main_lt->addWidget(text_lt);
-    this->setLayout(main_lt);
+    this->setLayout(main_lt);//вывод формы
 }
 
 // protected slots
-void AirspaceConverterInterface::Save_clicked(){
+void AirspaceConverterInterface::Save_clicked(){//функция сохранения файла
     QFileDialog::getSaveFileName();
 }
 void AirspaceConverterInterface::Combo_clicked(){
